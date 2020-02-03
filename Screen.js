328a1dd93bfef8a079604eefa4ab52014187bc2f
Tim Konieczny <timkonieczny@gmail.com>
2020-02-03 08:08:28 +0100

    Fixed TransitionOut not finishing for empty screens

--- Screen.js
+++ Screen.js
@@ -1,7 +1,6 @@
 import * as THREE from "three"
 
 class Screen {
-
     constructor(scene, camera) {
         this.objects = new Map()
         this.scene = scene
@@ -21,22 +20,35 @@ class Screen {
     }
 
     transitionIn(previousScreen, activeCharacter) {
+        console.log(`Transitioning in ${this.name} screen`)
         const checkObjectsAndFinishTransitionIn = _ => {
-            if (this.numberOfReadyNonSharedObjects == this.nonSharedObjects.length) {
+            if (
+                this.numberOfReadyNonSharedObjects ==
+                this.nonSharedObjects.length
+            ) {
                 this.numberOfReadyNonSharedObjects = 0
-                this.listeners.transitionInFinished.forEach(listener => { listener(this) })
+                this.listeners.transitionInFinished.forEach(listener => {
+                    listener(this)
+                })
             }
         }
 
-        this.nonSharedObjects = Array.from(this.objects.values()).filter(object =>
-            // objects that aren't shared
-            !object.isShared ||
-            // objects that are shared but this is their first screen (entry point)
-            object.isShared && !object.sharedBetween.get(previousScreen) ||
-            // objects that are shared, for this case: [screen before previous screen] shared with [previous screen] not shared with [this] shared with [next screen]
-            object.isShared && !object.sharedBetween.get(previousScreen).includes(this) && (object.entryPointScreens.indexOf(this) !== -1) ||
-            // objects that are shared, but were disabled and are now reappearing at their entry point
-            object.isShared && object.sharedBetween.get(previousScreen).includes(this) && (object.entryPointScreens.indexOf(this) !== -1) && this.scene.children.indexOf(object) === -1
+        this.nonSharedObjects = Array.from(this.objects.values()).filter(
+            object =>
+                // objects that aren't shared
+                !object.isShared ||
+                // objects that are shared but this is their first screen (entry point)
+                (object.isShared &&
+                    !object.sharedBetween.get(previousScreen)) ||
+                // objects that are shared, for this case: [screen before previous screen] shared with [previous screen] not shared with [this] shared with [next screen]
+                (object.isShared &&
+                    !object.sharedBetween.get(previousScreen).includes(this) &&
+                    object.entryPointScreens.indexOf(this) !== -1) ||
+                // objects that are shared, but were disabled and are now reappearing at their entry point
+                (object.isShared &&
+                    object.sharedBetween.get(previousScreen).includes(this) &&
+                    object.entryPointScreens.indexOf(this) !== -1 &&
+                    this.scene.children.indexOf(object) === -1)
         )
 
         this.numberOfReadyNonSharedObjects = 0
@@ -45,37 +57,91 @@ class Screen {
         this.nonSharedObjects.forEach(object => {
             this.scene.add(object)
             const onTransitionInFinished = _ => {
-                object.removeEventListener("transitionInFinished", onTransitionInFinished)
+                object.removeEventListener(
+                    "transitionInFinished",
+                    onTransitionInFinished
+                )
                 this.numberOfReadyNonSharedObjects++
                 checkObjectsAndFinishTransitionIn()
             }
-            object.addEventListener("transitionInFinished", onTransitionInFinished)
+            object.addEventListener(
+                "transitionInFinished",
+                onTransitionInFinished
+            )
             object.transition.time.elapsed = 0
             if (this.sharedObjectConfigs[object.name]) {
-                if (activeCharacter && this.sharedObjectConfigs[object.name][activeCharacter.name]) {
-                    if (this.sharedObjectConfigs[object.name][activeCharacter.name].position)
-                        object.position.copy(this.sharedObjectConfigs[object.name][activeCharacter.name].position)
-                    if (this.sharedObjectConfigs[object.name][activeCharacter.name].scale)
-                        object.scale.copy(this.sharedObjectConfigs[object.name][activeCharacter.name].scale)
+                if (
+                    activeCharacter &&
+                    this.sharedObjectConfigs[object.name][activeCharacter.name]
+                ) {
+                    if (
+                        this.sharedObjectConfigs[object.name][
+                            activeCharacter.name
+                        ].position
+                    )
+                        object.position.copy(
+                            this.sharedObjectConfigs[object.name][
+                                activeCharacter.name
+                            ].position
+                        )
+                    if (
+                        this.sharedObjectConfigs[object.name][
+                            activeCharacter.name
+                        ].scale
+                    )
+                        object.scale.copy(
+                            this.sharedObjectConfigs[object.name][
+                                activeCharacter.name
+                            ].scale
+                        )
                 }
                 if (this.sharedObjectConfigs[object.name].position)
-                    object.position.copy(this.sharedObjectConfigs[object.name].position)
+                    object.position.copy(
+                        this.sharedObjectConfigs[object.name].position
+                    )
                 if (this.sharedObjectConfigs[object.name].scale)
-                    object.scale.copy(this.sharedObjectConfigs[object.name].scale)
+                    object.scale.copy(
+                        this.sharedObjectConfigs[object.name].scale
+                    )
             }
             object.originalScale = object.scale.clone()
             object.addEventListener("update", object.updateTransitionIn)
         })
-        this.listeners.transitionInStarted.forEach(listener => { listener(this) })
+        this.listeners.transitionInStarted.forEach(listener => {
+            listener(this)
+        })
     }
 
     transitionOut(nextScreen, activeCharacter) {
-        this.listeners.transitionOutStarted.forEach(listener => { listener(this) })
+        console.log(`Transitioning out ${this.name} screen`)
 
-        this.nonSharedObjects = Array.from(this.objects.values()).filter(object => !object.sharedBetween.get(this).includes(nextScreen))
-        this.sharedObjects = Array.from(this.objects.values()).filter(object => object.sharedBetween.get(this).includes(nextScreen) && this.scene.children.indexOf(object) >= 0)
+        const checkObjectsAndFinishTransitionOut = _ => {
+            if (
+                this.numberOfReadyNonSharedObjects ==
+                this.nonSharedObjects.length
+            ) {
+                this.numberOfReadyNonSharedObjects = 0
+                this.listeners.transitionOutFinished.forEach(listener => {
+                    listener(this)
+                })
+            }
+        }
 
-        // console.log(this.nonSharedObjects)
+        this.listeners.transitionOutStarted.forEach(listener => {
+            listener(this)
+        })
+
+        this.nonSharedObjects = Array.from(this.objects.values()).filter(
+            object => !object.sharedBetween.get(this).includes(nextScreen)
+        )
+        this.sharedObjects = Array.from(this.objects.values()).filter(
+            object =>
+                object.sharedBetween.get(this).includes(nextScreen) &&
+                this.scene.children.indexOf(object) >= 0
+        )
+
+        this.numberOfReadyNonSharedObjects = 0
+        checkObjectsAndFinishTransitionOut()
 
         this.nonSharedObjects.forEach(object => {
             object.transition.time.elapsed = 0
@@ -85,7 +151,9 @@ class Screen {
                 this.scene.remove(object)
 
                 if (this.scene.children.length == this.sharedObjects.length) {
-                    this.listeners.transitionOutFinished.forEach(listener => { listener(this) })
+                    this.listeners.transitionOutFinished.forEach(listener => {
+                        listener(this)
+                    })
                 }
             })
         })
@@ -94,31 +162,51 @@ class Screen {
             object.transition.time.elapsed = 0
             object.transition.screen.from = this
             object.transition.screen.to = nextScreen
-            object.transition.character = activeCharacter ? activeCharacter.name : activeCharacter
-            object.addEventListener("update", object.updateSharedScreenTransition)
+            object.transition.character = activeCharacter
+                ? activeCharacter.name
+                : activeCharacter
+            object.addEventListener(
+                "update",
+                object.updateSharedScreenTransition
+            )
         })
         this.camera.transition.time.elapsed = 0
         this.camera.transition.screen.from = this
         this.camera.transition.screen.to = nextScreen
-        this.camera.addEventListener("update", this.camera.updateSharedScreenTransition)
+        this.camera.addEventListener(
+            "update",
+            this.camera.updateSharedScreenTransition
+        )
     }
 
-    addSharedObject(sharedObject, screen, sharedWithScreens) {    // TODO: split this up into two external calls for clarity
+    addSharedObject(sharedObject, screen, sharedWithScreens) {
+        // TODO: split this up into two external calls for clarity
         this.add(sharedObject, screen)
-        this.objects.get(sharedObject.name).sharedBetween.set(screen, sharedWithScreens)
+        this.objects
+            .get(sharedObject.name)
+            .sharedBetween.set(screen, sharedWithScreens)
     }
 
-    removeSharedObject(sharedObject, sharedWithScreen) {                 // TODO: after splitting addSharedObject up, rename to removeObjectScreenSharingConnection
-        const sharedWithScreens = this.objects.get(sharedObject.name).sharedBetween.get(this)
+    removeSharedObject(sharedObject, sharedWithScreen) {
+        // TODO: after splitting addSharedObject up, rename to removeObjectScreenSharingConnection
+        const sharedWithScreens = this.objects
+            .get(sharedObject.name)
+            .sharedBetween.get(this)
         sharedWithScreens.splice(sharedWithScreens.indexOf(sharedWithScreen), 1)
     }
 
     update(tslf) {
-        this.listeners.update.forEach(listener => { listener(this, tslf) })
-        this.objects.forEach(object => {
-            object.listeners.update.forEach(listener => { listener(tslf, object) })
+        this.listeners.update.forEach(listener => {
+            listener(this, tslf)
         })
-        this.camera.listeners.update.forEach(listener => { listener(tslf, this.camera) })
+        this.objects.forEach(object => {
+            object.listeners.update.forEach(listener => {
+                listener(tslf, object)
+            })
+        })
+        this.camera.listeners.update.forEach(listener => {
+            listener(tslf, this.camera)
+        })
     }
 
     addEventListener(type, listener) {
@@ -146,9 +234,16 @@ class Screen {
     }
 
     moveObjectTransformationOriginToCenter(geometry) {
-        const temp = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: '#ffffff' }))
-        const boundingBox = new THREE.Box3().setFromObject(temp);
-        const centerMatrix = new THREE.Matrix4().makeTranslation(-boundingBox.max.x / 2, -boundingBox.max.y / 2, 0)
+        const temp = new THREE.Mesh(
+            geometry,
+            new THREE.MeshBasicMaterial({ color: "#ffffff" })
+        )
+        const boundingBox = new THREE.Box3().setFromObject(temp)
+        const centerMatrix = new THREE.Matrix4().makeTranslation(
+            -boundingBox.max.x / 2,
+            -boundingBox.max.y / 2,
+            0
+        )
         geometry.applyMatrix(centerMatrix)
         return geometry
     }
