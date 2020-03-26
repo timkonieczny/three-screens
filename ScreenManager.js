e6619db256c52cb4573b5128ec6a630560271b56
Tim Konieczny <timkonieczny@gmail.com>
2020-03-26 08:59:04 +0100

    Enabled changing target screen while transitionOut is in progress

--- ScreenManager.js
+++ ScreenManager.js
@@ -1,6 +1,7 @@
 class ScreenManager {
     constructor(activeScreen) {
         this.isTransitionInProgress = false
+        this.nextScreen = null
         if (activeScreen) this.transitionTo(activeScreen)
     }
 
@@ -22,41 +23,44 @@ class ScreenManager {
     }
 
     transitionTo(screen, activeCharacter = null) {
-        if (!this.isTransitionInProgress && screen !== this.activeScreen) {
-            this.isTransitionInProgress = true
-            const onTransitionOutFinished = _ => {
-                const previousScreen = this.activeScreen
-                this.activeScreen = screen
-                this.activeScreen.addEventListener("transitionInFinished", onTransitionInFinished)
-                this.activeScreen.transitionIn(previousScreen, activeCharacter)
-                if (previousScreen)
-                    previousScreen.removeEventListener(
-                        "transitionOutFinished",
-                        onTransitionOutFinished
+        if (screen !== this.activeScreen) {
+            this.nextScreen = screen
+            if (!this.isTransitionInProgress) {
+                this.isTransitionInProgress = true
+                const onTransitionOutFinished = _ => {
+                    const previousScreen = this.activeScreen
+                    this.activeScreen = this.nextScreen
+                    this.activeScreen.addEventListener("transitionInFinished", onTransitionInFinished)
+                    this.activeScreen.transitionIn(previousScreen, activeCharacter)
+                    if (previousScreen)
+                        previousScreen.removeEventListener(
+                            "transitionOutFinished",
+                            onTransitionOutFinished
+                        )
+                    this.isTransitionInProgress = false
+                }
+                const onTransitionInFinished = _ => {
+                    // This just needs to be called, so that shared objects
+                    // that weren't shared with the previous screen, but with
+                    // screens before that, are set visible again. They were
+                    // set invisible after they were transitioned out.
+
+                    this.activeScreen.objects.forEach(object => {
+                        if (object.visibleOverride === null) object.visible = true
+                        else object.visible = object.visibleOverride
+                    })
+                    this.activeScreen.removeEventListener(
+                        "transitionInFinished",
+                        onTransitionInFinished
                     )
-                this.isTransitionInProgress = false
-            }
-            const onTransitionInFinished = _ => {
-                // This just needs to be called, so that shared objects
-                // that weren't shared with the previous screen, but with
-                // screens before that, are set visible again. They were
-                // set invisible after they were transitioned out.
+                }
 
-                this.activeScreen.objects.forEach(object => {
-                    if (object.visibleOverride === null) object.visible = true
-                    else object.visible = object.visibleOverride
-                })
-                this.activeScreen.removeEventListener(
-                    "transitionInFinished",
-                    onTransitionInFinished
-                )
-            }
-
-            if (this.activeScreen) {
-                this.activeScreen.addEventListener("transitionOutFinished", onTransitionOutFinished)
-                this.activeScreen.transitionOut(screen, activeCharacter)
-            } else {
-                onTransitionOutFinished()
+                if (this.activeScreen) {
+                    this.activeScreen.addEventListener("transitionOutFinished", onTransitionOutFinished)
+                    this.activeScreen.transitionOut(this.nextScreen, activeCharacter)
+                } else {
+                    onTransitionOutFinished()
+                }
             }
         }
     }
